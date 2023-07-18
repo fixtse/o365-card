09b47c6691dfffbd60dc3a1113f585a814070c96
Fixt <fixtse@gmail.com>
2023-07-18 01:01:06 -0500

    build: :sparkles: first version

---
+++ README.md
@@ -0,0 +1,89 @@
+<a name="readme-top"></a>
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-41BDF5.svg?style=for-the-badge)](https://github.com/hacs/integration)
+
+# Office 365 Card
+
+Table of contents
+-----------------
+
+* [Introduction](#introduction)
+* [Default Config](#default-config)
+* [Supported Types](#supported-types)
+  * [Inbox](#inbox)
+  * [To Do](#to-do)
+  * [Teams](#teams)
+
+## Introduction
+Needs the [Office 365 Integration](https://github.com/RogerSelwyn/O365-HomeAssistant) to work
+
+This is a Card to show information from your Office 365 sensors into your home assistant dashboard. The intend its to create a <b>simple</b> and easy to use card to integrate the sensors information in the dashboard.
+
+I made a video about the Office 365 integration and on my research, I couldn't find any other card that allowed me to show the information from the sensors easily. So I made this card to simplify the process for everyone that needs it.
+
+The Icon and the name of the card are taken from the sensor's configuration
+
+Once installed can be added from the Dashboard Panel, just search for Office365 Card
+
+## Default Config
+
+```yaml
+type: custom:office365-card
+entity: sensor.inbox
+max_items: 4
+only_overdue: false
+```
+## Supported Types
+
+## Inbox
+<p align="center"><img src="img/inbox.webp" alt="inbox sensor example img"></p>
+
+* The card shows the email list from an email sensor ([doc](https://rogerselwyn.github.io/O365-HomeAssistant/installation_and_configuration.html#email_sensors)) or a query sensor ([doc](https://rogerselwyn.github.io/O365-HomeAssistant/installation_and_configuration.html#query_sensors))
+* State Color based on importance level of the email
+  * Info: Normal
+  * Alert: Important
+
+#### Options
+| Name  | Requirement | Description | 
+| --- | --- |  --- |
+| type  | **Required** | `custom:office365-card` |
+| entity | **Required**  | Home Assistant entity ID |
+| max_items | Optional | Maximum amount of items to show in the card <br> The header counter will still show the real value <br> Set to 0 to show all  |
+
+## To Do
+<p align="center"><img src="img/task.webp" alt="task sensor example img"></p>
+
+* The card Lists the items from a tasks sensor ([doc](https://rogerselwyn.github.io/O365-HomeAssistant/sensor.html#taskto-do-sensor))
+* The sensor only retrieve not finished tasks, so there is no way to show the finished ones
+* Each item has a link that will take you to the items page on todo.office.com
+* The State Color is based on the Due Date:
+  * Warning : With Due Date, not overdue
+  * Alert: With Due Date, overdue
+  * Info: Without Due Date
+
+#### Options
+| Name | Requirement | Description | 
+| --- | --- | --- |
+| type  | **Required** | `custom:office365-card` |
+| entity | **Required**  | Home Assistant entity ID |
+| max_items | Optional | Maximum amount of items to show in the card <br> The header counter will still show the real value <br> Set to 0 to show all  |
+| only_overdue | Optional | Only show overdue tasks <br> Default: false |
+
+## Teams
+<p align="center"><img src="img/teams_last_message.webp" alt="task sensor example img"></p>
+
+* The card shows the information from the teams chat sensor ([doc](https://rogerselwyn.github.io/O365-HomeAssistant/sensor.html#teams-chat-sensor))
+* Has a link that will open the conversation in teams.microsoft.com
+
+#### Options
+| Name | Requirement | Description | 
+| --- | --- | --- |
+| type  | **Required** | `custom:office365-card` |
+| entity | **Required**  | Home Assistant entity ID |
+
+
+## Licence
+office365-card is [MIT licenced](license.txt)
+
+
+
---
+++ office365-card.js
@@ -0,0 +1,242 @@
+import {
+  LitElement,
+  html,
+  css,
+} from "https://unpkg.com/lit-element@3.3.2/lit-element.js?module";
+
+
+class Office365MailTodoView extends LitElement {
+  static get properties() {
+    return {
+      hass: {},
+      config: {},
+    };
+  }
+
+  render() {
+    const entityId = this.config.entity;
+    let max_item = this.config.max_items;
+    
+    if (!!this.hass.states[entityId]){
+        if(!this.hass.states[entityId].attributes['data']) {
+            // Tasks
+            let attributeValue;
+            if(this.config.only_overdue == true){
+                attributeValue = this.hass.states[entityId].attributes['overdue_tasks'];
+            }else{
+                attributeValue = this.hass.states[entityId].attributes['all_tasks'];
+            }
+            
+            if (max_item == 0){
+                max_item = attributeValue.size;
+            }
+            let card_type; 
+            return attributeValue ? html`
+              <ha-card class="card">
+                <div class='title_tasks'>
+                    <ha-icon icon="${this.hass.states[entityId].attributes['icon']}"></ha-icon> ${this.hass.states[entityId].attributes['friendly_name']} (${this.hass.states[entityId].state})
+                </div>
+                <ul class="separator">
+                ${attributeValue.slice(0,max_item).map(entry => {
+                    if (!entry.due){
+                        card_type="info"; 
+                        return entry.subject ? html`           
+                                 <li>
+                                    <ha-alert alert-type=${card_type} class="tasks">${entry.subject} 
+                                        <a href="https://to-do.office.com/tasks/id/${entry.task_id}" target="_blank">&#128279;</a>
+                                     </ha-alert>
+                                 </li>`: html`<li>
+                                                <ha-card class="not-found">
+                                                       <ha-alert alert-type="warning">Entity <b>${entityId}</b> is not a valid o365 task item.</ha-alert>
+                                                </ha-card>
+                                              </li>`;
+                        
+                    }else{
+                        let now = new Date().getTime();
+                        let due = new Date(entry.due);
+                        let options = {year: 'numeric', day:'numeric', month:'short'};
+                        
+                        //Its overdue?
+                        if(now < due.getTime()){  
+                            //no
+                            card_type="warning";
+                        }else{
+                           //yes
+                           card_type="error";
+                        }
+                        return entry.subject ? html`           
+                                 <li><ha-alert alert-type=${card_type} class="tasks">${entry.subject} 
+                                        <a href="https://to-do.office.com/tasks/id/${entry.task_id}" target="_blank">&#128279;</a>
+                                            <div>Due date: <b>${due.toLocaleDateString("en-US", options)}</b></div>
+                                     </ha-alert>
+                                 </li>`: html`<li>
+                                                <ha-card class="not-found">
+                                                       <ha-alert alert-type="warning">Entity <b>${entityId}</b> is not a valid o365 task item.</ha-alert>
+                                                </ha-card>
+                                              </li>`;
+                    }
+                    
+                })}
+                </ul>
+              </ha-card>
+            `: html`<ha-card class="not-found">
+                        <ha-alert alert-type="warning">Entity <b>${entityId}</b> is not a valid o365 sensor.</ha-alert>
+                    </ha-card>`;
+        }else  if (!this.hass.states[entityId].attributes['all_tasks']){
+            if (!this.hass.states[entityId].attributes['from_display_name']){
+                //Email
+                const attributeValue = this.hass.states[entityId].attributes['data'];
+                if (max_item == 0){
+                    max_item = attributeValue.size;
+                }
+                return attributeValue ? html`
+                  <ha-card class="card">
+                    <div class='title'>
+                        <ha-icon icon="${this.hass.states[entityId].attributes['icon']}"></ha-icon> ${this.hass.states[entityId].attributes['friendly_name']} (${this.hass.states[entityId].state})
+                    </div>
+                    ${attributeValue.slice(0,max_item).map(entry => {
+                            let t = new Date(entry.received);
+                            let options = {year: 'numeric', day:'numeric', month:'short', hour:'2-digit', minute:'2-digit'};
+                            let card_type="info"; 
+                            if (entry.importance != "normal"){
+                                al_type="error"; 
+                            }
+                             return entry.subject ? html`   
+                                 <div class="state">
+                                     <ha-alert  alert-type=${card_type}>
+                                         <div>
+                                            <b>${entry.subject}</b>
+                                         </div> 
+                                         <div>
+                                            <b>From: </b><a href="mailto:${entry.sender}">${entry.sender}</a>
+                                         </div>
+                                         <div>
+                                            <b>Date: </b>${t.toLocaleDateString("en-US", options)}
+                                         </div>
+                                     </ha-alert>
+                                 </div>`
+                                 : html`<ha-card class="not-found">
+                                            <ha-alert alert-type="warning">Entity <b>${entityId}</b> is not a valid o365 mail item.</ha-alert>
+                                        </ha-card>`;
+                        })}
+                  </ha-card>
+                `: html`<ha-card class="not-found">
+                        <ha-alert alert-type="warning">Entity <b>${entityId}</b> is not a valid o365 sensor.</ha-alert>
+                    </ha-card>`;
+            }else{
+                //Teams Message
+                let t = new Date(this.hass.states[entityId].state);
+                let options = {year: 'numeric', day:'numeric', month:'short', hour:'2-digit', minute:'2-digit'};
+                return html`
+                <ha-card class="card">
+                    <div class='title'>
+                        <ha-icon icon="${this.hass.states[entityId].attributes['icon']}"></ha-icon> ${this.hass.states[entityId].attributes['friendly_name']} Last Message
+                    </div>
+                    <div class="list"><b>From: </b>${this.hass.states[entityId].attributes['from_display_name']} </div>
+                    <div class="list"><b>At: </b>${t.toLocaleDateString("en-US", options)}</div>
+                    <div class="list"><b>Message: </b>${this.hass.states[entityId].attributes['content'].replace(/(<([^>]+)>)/gi, "")}</div>
+                    <br>
+                    <div class="list">Open in <a href="https://teams.microsoft.com/_#/conversations/${  this.hass.states[entityId].attributes['chat_id']}?ctx=chat" target="_blank">Teams</a></div>
+                </ha-card>`;
+            }
+            
+        }
+            
+    }else{
+        return html`<ha-card class="not-found">
+                    <ha-alert alert-type="error">Entity <b>${entityId}</b> not found.</ha-alert>
+                </ha-card>`;
+    }
+    
+  }
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error("You need to define an entity");
+    }
+    this.config = config;
+  }
+
+  
+  getCardSize() {
+    let size;
+    if ( typeOf(this.config.max_items) != "unavailable"){
+        if (this.config.max_items == 0){
+            size = this.config.entity.state + 1;
+        }else{
+            size = this.config.max_items + 1;
+        }
+    }else{
+        size = this.config.entity.state + 1;
+    }
+    return size;
+  }
+
+  static getStubConfig() {
+    return { entity: "sensor.inbox", max_items: 4, only_overdue: false }
+  }
+
+
+  static get styles() {
+    return css`
+      :host {
+        
+      }
+      .title_tasks {
+          font-weight: bold;
+          font-size: 20px;
+          text-transform: uppercase;
+          padding: 6px 0 0 0;
+        }
+      .title {
+          font-weight: bold;
+          font-size: 20px;
+          text-transform: uppercase;
+          padding: 6px 0 16px 0;
+        }
+      ul.separator {
+      	list-style: none;
+      	padding: 0;
+      	width: 100%;
+      }
+        
+      ul.separator li {
+        padding: .5em 0 0 0;
+      	
+      }
+      ul.separator li:last-child {
+        border-bottom: 0;
+      }
+      a{
+        color: #339CFF;
+      }
+      .card{
+          padding: 16px;
+      }
+      .state {
+        justify-content: space-between;
+        padding: 8px;
+        align-items: center;
+      }
+      .list {
+        padding: 5px 1em 0 2em;
+      }
+      .tasks {
+        padding: 0px 0em 0px 0px;
+      }
+      .not-found {
+        font-size: 14px;
+        padding: 8px;
+      }
+    `;
+  }
+}
+customElements.define('office365-card', Office365MailTodoView);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+    type: "office365-card",
+    name: "Office365 Card",
+    description: "A custom card to show your Inbox, To Do and Teams Last Message from Office365" // optional
+});
+
